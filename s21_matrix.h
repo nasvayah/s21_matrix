ffd0dc08407780d65392f80818eff0a32d139751
Varvara <varvaraparamon@gmail.com>
2024-06-18 17:39:59 +0300

    sdds

--- s21_matrix.h
+++ s21_matrix.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define OK 0
 #define INCORRECT_MATRIX 1
 #define CALC_ERROR 2
@@ -16,5 +17,13 @@ typedef struct matrix_struct {
 } matrix_t;
 
 int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A);
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+
+void s21_fill_matrix(matrix_t *result);
+void s21_print_matrix(matrix_t *result);
+int s21_iscorrect_matrix(matrix_t *A);
 
 #endif
